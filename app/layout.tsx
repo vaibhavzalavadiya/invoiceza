3d0d1eb8f06381a21807ce1a7af0837e04993509
vaibhavzalavadiya <vaibhavzalavadiya@ganil.com>
2026-02-20 19:48:22 +0530

    clarity add 19-2-26

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -162,6 +163,16 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+         <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+        >
+          {`(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "vkd890fga2");`}
+        </Script>
       </body>
     </html>
   );
